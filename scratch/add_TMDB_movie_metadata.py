3b8c19fed10dbe4e4b2267cd3591ada6c80fd512
Nuimo <15246981+Nuimo@users.noreply.github.com>
2023-11-01 16:58:37 +0100

    scrape 99% working, just needsto run for approx 8 hours

--- scratch/add_TMDB_movie_metadata.py
+++ scratch/add_TMDB_movie_metadata.py
@@ -24,12 +24,12 @@ tmdb.REQUESTS_TIMEOUT = 5  # seconds, for both connect and read
 saved_progress = []
 
 # Determine where to resume (if applicable)
-start_index = 5  # You can adjust this if you need to start from a specific index
+start_index = 0  # You can adjust this if you need to start from a specific index
 
 # If you have a saved progress file, you can use it to resume
 # For example, if you have a 'progress.json' file containing previously processed indices:
-# saved_progress = pd.read_json('progress.json')['index'].tolist()
-# start_index = saved_progress[-1] + 1  # Start from the next index
+saved_progress = pd.read_json('progress.json')['index'].tolist()
+start_index = saved_progress[-1] + 1  # Start from the next index
 
 # Create a DataFrame to store your data
 movie_metadata_TMDB = movie_metadata.copy()
@@ -55,11 +55,18 @@ for index, row in tqdm(movie_metadata_TMDB.iterrows(), total=len(movie_metadata_
                 movie_release_date = datetime.strptime(movie_release_date_str, "%Y-%m-%d").date()
             # convert query elements to datetime
 
+
+            #date_list_converted = [datetime.strptime(each_date['release_date'], "%Y-%m-%d").date() for each_date in search.results]
+
+            # handle if release_date is empty.
             date_list_converted = [datetime.strptime(each_date['release_date'], "%Y-%m-%d").date() for each_date in
-                                   search.results]
+                                   search.results if each_date.get('release_date')]
 
             # create list of differences in time
             differences = [abs(movie_release_date - each_date) for each_date in date_list_converted]
+            # if differences is empty skip
+            if not differences:
+                continue
             minimum_index = differences.index(min(differences))  # Index of the closest match
             match = search.results[minimum_index]
             # print(f"Closest match: {match['title']} (Release Date: {match['release_date']})")
@@ -80,14 +87,15 @@ for index, row in tqdm(movie_metadata_TMDB.iterrows(), total=len(movie_metadata_
 
             # Save progress periodically (in case of interruption)
             if index % 50 == 0:
+                movie_metadata_TMDB.to_csv('movie_metadata_TMDB.csv', index=False)
                 progress_df = pd.DataFrame({'index': saved_progress})
                 progress_df.to_json('progress.json')
 
     except Exception as e:
         print(f"Error at index {index}: {str(e)}")
-
-    if index == 10:
-        break
+    #print("index:",index)
+    #if index >= 105:
+    #    break
 
 # Save final progress
 progress_df = pd.DataFrame({'index': saved_progress})
