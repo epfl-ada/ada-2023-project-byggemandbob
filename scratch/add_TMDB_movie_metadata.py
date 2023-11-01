8892dbfbc6e2bc5abed29740528f3c1381e7b569
Nuimo <15246981+Nuimo@users.noreply.github.com>
2023-11-01 16:24:31 +0100

    implemented progress bar, error handling and continuing if failed

--- scratch/add_TMDB_movie_metadata.py
+++ scratch/add_TMDB_movie_metadata.py
@@ -24,7 +24,7 @@ tmdb.REQUESTS_TIMEOUT = 5  # seconds, for both connect and read
 saved_progress = []
 
 # Determine where to resume (if applicable)
-start_index = 0  # You can adjust this if you need to start from a specific index
+start_index = 5  # You can adjust this if you need to start from a specific index
 
 # If you have a saved progress file, you can use it to resume
 # For example, if you have a 'progress.json' file containing previously processed indices:
@@ -40,52 +40,54 @@ for index, row in tqdm(movie_metadata_TMDB.iterrows(), total=len(movie_metadata_
         continue
 
     try:
-        if ~(row["Movie release date"].isnan()):
-            break
-        search = tmdb.Search()
-        response = search.movie(query=row["Movie name"])
+        if not pd.isna(row["Movie release date"]):
+            search = tmdb.Search()
+            response = search.movie(query=row["Movie name"])
 
-        # convert dataframe release to datetime
-        #movie_release_date = datetime.strptime(row["Movie release date"], "%Y-%m-%d").date()
-        movie_release_date_str = row["Movie release date"]
-        if len(movie_release_date_str) == 4:  # Handle "YYYY" format
-            movie_release_date = datetime.strptime(movie_release_date_str, "%Y").date()
-        elif len(movie_release_date_str) == 7:  # Handle "YYYY-DD" format
-            movie_release_date = datetime.strptime(movie_release_date_str, "%Y-%m").date()
-        else:  # Assume it's in the format "YYYY-MM-DD"
-            movie_release_date = datetime.strptime(movie_release_date_str, "%Y-%m-%d").date()
-        # convert query elements to datetime
+            # convert dataframe release to datetime
+            #movie_release_date = datetime.strptime(row["Movie release date"], "%Y-%m-%d").date()
+            movie_release_date_str = row["Movie release date"]
+            if len(movie_release_date_str) == 4:  # Handle "YYYY" format
+                movie_release_date = datetime.strptime(movie_release_date_str, "%Y").date()
+            elif len(movie_release_date_str) == 7:  # Handle "YYYY-DD" format
+                movie_release_date = datetime.strptime(movie_release_date_str, "%Y-%m").date()
+            else:  # Assume it's in the format "YYYY-MM-DD"
+                movie_release_date = datetime.strptime(movie_release_date_str, "%Y-%m-%d").date()
+            # convert query elements to datetime
 
-        date_list_converted = [datetime.strptime(each_date['release_date'], "%Y-%m-%d").date() for each_date in
-                               search.results]
+            date_list_converted = [datetime.strptime(each_date['release_date'], "%Y-%m-%d").date() for each_date in
+                                   search.results]
 
-        # create list of differences in time
-        differences = [abs(movie_release_date - each_date) for each_date in date_list_converted]
-        minimum_index = differences.index(min(differences))  # Index of the closest match
-        match = search.results[minimum_index]
-        # print(f"Closest match: {match['title']} (Release Date: {match['release_date']})")
+            # create list of differences in time
+            differences = [abs(movie_release_date - each_date) for each_date in date_list_converted]
+            minimum_index = differences.index(min(differences))  # Index of the closest match
+            match = search.results[minimum_index]
+            # print(f"Closest match: {match['title']} (Release Date: {match['release_date']})")
 
-        # add info in dataframe about the movie
+            # add info in dataframe about the movie
 
-        movie_metadata_TMDB.loc[index, 'TMDB_id'] = match['id']
-        movie_metadata_TMDB.loc[index, 'TMDB_original_language'] = match['original_language']
-        movie_metadata_TMDB.loc[index, 'TMDB_original_title'] = match['original_title']
-        movie_metadata_TMDB.loc[index, 'TMDB_overview'] = match['overview']
-        movie_metadata_TMDB.loc[index, 'TMDB_popularity'] = match['popularity']
-        movie_metadata_TMDB.loc[index, 'TMDB_release_date'] = match['release_date']
-        movie_metadata_TMDB.loc[index, 'TMDB_title'] = match['title']
-        movie_metadata_TMDB.loc[index, 'TMDB_vote_average'] = match['vote_average']
-        movie_metadata_TMDB.loc[index, 'TMDB_vote_count'] = match['vote_count']
-        # Save the index as progress
-        saved_progress.append(index)
+            movie_metadata_TMDB.loc[index, 'TMDB_id'] = match['id']
+            movie_metadata_TMDB.loc[index, 'TMDB_original_language'] = match['original_language']
+            movie_metadata_TMDB.loc[index, 'TMDB_original_title'] = match['original_title']
+            movie_metadata_TMDB.loc[index, 'TMDB_overview'] = match['overview']
+            movie_metadata_TMDB.loc[index, 'TMDB_popularity'] = match['popularity']
+            movie_metadata_TMDB.loc[index, 'TMDB_release_date'] = match['release_date']
+            movie_metadata_TMDB.loc[index, 'TMDB_title'] = match['title']
+            movie_metadata_TMDB.loc[index, 'TMDB_vote_average'] = match['vote_average']
+            movie_metadata_TMDB.loc[index, 'TMDB_vote_count'] = match['vote_count']
+            # Save the index as progress
+            saved_progress.append(index)
 
-        # Save progress periodically (in case of interruption)
-        if index % 50 == 0:
-            progress_df = pd.DataFrame({'index': saved_progress})
-            progress_df.to_json('progress.json')
+            # Save progress periodically (in case of interruption)
+            if index % 50 == 0:
+                progress_df = pd.DataFrame({'index': saved_progress})
+                progress_df.to_json('progress.json')
 
     except Exception as e:
         print(f"Error at index {index}: {str(e)}")
+
+    if index == 10:
+        break
 
 # Save final progress
 progress_df = pd.DataFrame({'index': saved_progress})
